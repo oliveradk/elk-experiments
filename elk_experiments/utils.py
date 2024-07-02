fd526c08e5a71ce31c8ef31bac1398c24a35e7b1
Oliver Daniels-Koch <danielskoch.oliver@gmail.com>
2024-07-02 13:05:22 -0400

    various submodule updates, 90% auroc on ifelse using EAP, nb for aggregate graphs

--- elk_experiments/utils.py
+++ elk_experiments/utils.py
@@ -8,6 +8,9 @@ from cupbearer.detectors.anomaly_detector import AnomalyDetector
 from cupbearer.detectors.activation_based import ActivationCache
 from cupbearer.utils import SUFFIX
 
+from eap.eap_graph import EAPGraph 
+from eap.eap_wrapper import EAP
+
 
 def train_detector_cache(
         detector_dir: str, 
@@ -34,4 +37,34 @@ def train_detector_cache(
             os.remove(cache_path)
         cache.store(cache_path)
     return out
-        
+
+def learn_graph_cache(
+    model,
+    tokens,
+    metric,
+    upstream_nodes,
+    downstream_nodes,
+    batch_size,
+    cache_path,
+    verbose=False,
+    overwrite=False,
+):
+    if cache_path.is_file() and not overwrite:
+        graph = EAPGraph(
+            model.cfg, 
+            upstream_nodes=upstream_nodes,
+            downstream_nodes=downstream_nodes,
+        )
+        graph.load_scores(cache_path)
+    else:
+        graph = EAP(
+            model=model,
+            clean_tokens=tokens,
+            metric=metric,
+            upstream_nodes=upstream_nodes,
+            downstream_nodes=downstream_nodes,
+            batch_size=batch_size,
+            verbose=verbose,
+        )
+        graph.save_scores(cache_path)
+    return graph
