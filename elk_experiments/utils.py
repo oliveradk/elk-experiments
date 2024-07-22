19487cc3212d409bdba50b0472c225a303dab571
Oliver Daniels-Koch <danielskoch.oliver@gmail.com>
2024-07-22 10:23:16 -0400

    return model in set model and comment out cache utils

--- elk_experiments/utils.py
+++ elk_experiments/utils.py
@@ -9,7 +9,6 @@ from transformer_lens import HookedTransformer
 
 from cupbearer import tasks, detectors, scripts
 from cupbearer.detectors.anomaly_detector import AnomalyDetector
-from cupbearer.detectors.activation_based import ActivationCache
 from cupbearer.utils import SUFFIX
 
 from eap.eap_graph import EAPGraph 
@@ -23,64 +22,65 @@ def set_model(model: HookedTransformer, disbale_grad: bool = True):
     if disbale_grad:
         for param in model.parameters():
             param.requires_grad = False
+    return model
 
 
-def train_detector_cache(
-        detector_dir: str, 
-        detector: AnomalyDetector, 
-        task: tasks.Task, 
-        batch_size, 
-        eval_batch_size,
-        cache: ActivationCache = None,
-        cache_path: str = None,
-        overwrite=False,
-        overwrite_cache=False,
-        **train_kwargs
-):  
-    detector_dir = Path(detector_dir)
-    if os.path.exists(detector_dir) and not overwrite:
-        detector.load_weights(os.path.join(detector_dir, "detector.pth"))
-        out = scripts.eval_detector(task, detector, save_path=None, batch_size=eval_batch_size, pbar=True)
-    else:
-        out = scripts.train_detector(
-            task, detector, save_path=detector_dir, batch_size=batch_size, eval_batch_size=eval_batch_size, **train_kwargs
-        )
-    if overwrite_cache and cache is not None and cache_path is not None:
-        if os.path.exists(cache_path): # remove old cache
-            os.remove(cache_path)
-        cache.store(cache_path)
-    return out
+# def train_detector_cache(
+#         detector_dir: str, 
+#         detector: AnomalyDetector, 
+#         task: tasks.Task, 
+#         batch_size, 
+#         eval_batch_size,
+#         cache: ActivationCache = None,
+#         cache_path: str = None,
+#         overwrite=False,
+#         overwrite_cache=False,
+#         **train_kwargs
+# ):  
+#     detector_dir = Path(detector_dir)
+#     if os.path.exists(detector_dir) and not overwrite:
+#         detector.load_weights(os.path.join(detector_dir, "detector.pth"))
+#         out = scripts.eval_detector(task, detector, save_path=None, batch_size=eval_batch_size, pbar=True)
+#     else:
+#         out = scripts.train_detector(
+#             task, detector, save_path=detector_dir, batch_size=batch_size, eval_batch_size=eval_batch_size, **train_kwargs
+#         )
+#     if overwrite_cache and cache is not None and cache_path is not None:
+#         if os.path.exists(cache_path): # remove old cache
+#             os.remove(cache_path)
+#         cache.store(cache_path)
+#     return out
 
-def learn_graph_cache(
-    model,
-    tokens,
-    metric,
-    upstream_nodes,
-    downstream_nodes,
-    batch_size,
-    cache_path,
-    verbose=False,
-    overwrite=False,
-):
-    if cache_path.is_file() and not overwrite:
-        graph = EAPGraph(
-            model.cfg, 
-            upstream_nodes=upstream_nodes,
-            downstream_nodes=downstream_nodes,
-        )
-        graph.load_scores(cache_path)
-    else:
-        graph = EAP(
-            model=model,
-            clean_tokens=tokens,
-            metric=metric,
-            upstream_nodes=upstream_nodes,
-            downstream_nodes=downstream_nodes,
-            batch_size=batch_size,
-            verbose=verbose,
-        )
-        graph.save_scores(cache_path)
-    return graph
+# def learn_graph_cache(
+#     model,
+#     tokens,
+#     metric,
+#     upstream_nodes,
+#     downstream_nodes,
+#     batch_size,
+#     cache_path,
+#     verbose=False,
+#     overwrite=False,
+# ):
+#     if cache_path.is_file() and not overwrite:
+#         graph = EAPGraph(
+#             model.cfg, 
+#             upstream_nodes=upstream_nodes,
+#             downstream_nodes=downstream_nodes,
+#         )
+#         graph.load_scores(cache_path)
+#     else:
+#         graph = EAP(
+#             model=model,
+#             clean_tokens=tokens,
+#             metric=metric,
+#             upstream_nodes=upstream_nodes,
+#             downstream_nodes=downstream_nodes,
+#             batch_size=batch_size,
+#             verbose=verbose,
+#         )
+#         graph.save_scores(cache_path)
+#     return graph
 
 def get_activation_at_last_token(
     activation: torch.Tensor, inputs: list[list[int]], name: str
