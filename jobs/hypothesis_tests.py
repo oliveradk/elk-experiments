7b375572f427ca61dc2bc478a09c892437901e6d
oliveradk <danielskoch.oliver@gmail.com>
2024-08-07 22:48:57 -0700

    full job configuration

--- jobs/hypothesis_tests.py
+++ jobs/hypothesis_tests.py
@@ -17,69 +17,69 @@ from auto_circuit.tasks import (
 from auto_circuit.types import AblationType
 from elk_experiments.auto_circuit.score_funcs import GradFunc, AnswerFunc
 
-# specify the hyperparameters
-# tasks = [
-#     IOI_TOKEN_CIRCUIT_TASK, 
-#     IOI_COMPONENT_CIRCUIT_TASK, 
-#     DOCSTRING_TOKEN_CIRCUIT_TASK, 
-#     DOCSTRING_COMPONENT_CIRCUIT_TASK, 
-#     GREATERTHAN_COMPONENT_CIRCUIT_TASK, 
-#     # IOI_GPT2_AUTOENCODER_COMPONENT_CIRCUIT_TASK, 
-#     # GREATERTHAN_GPT2_AUTOENCODER_COMPONENT_CIRCUIT_TASK, 
-#     # ANIMAL_DIET_GPT2_AUTOENCODER_COMPONENT_CIRCUIT_TASK
-# ]
-# use_abs = [True, False]
-# ablation_types = [
-#     AblationType.RESAMPLE, 
-#     AblationType.TOKENWISE_MEAN_CLEAN,
-#     AblationType.TOKENWISE_MEAN_CORRUPT, 
-#     AblationType.TOKENWISE_MEAN_CLEAN_AND_CORRUPT,
-#     AblationType.ZERO
-# ]
-# grad_funcs = [
-#     GradFunc.LOGIT, 
-#     GradFunc.LOGPROB
-# ]
-# answer_funcs = [
-#     AnswerFunc.MAX_DIFF, 
-#     AnswerFunc.AVG_VAL
-# ]
+#specify the hyperparameters
+tasks = [
+    IOI_TOKEN_CIRCUIT_TASK, 
+    IOI_COMPONENT_CIRCUIT_TASK, 
+    DOCSTRING_TOKEN_CIRCUIT_TASK, 
+    DOCSTRING_COMPONENT_CIRCUIT_TASK, 
+    GREATERTHAN_COMPONENT_CIRCUIT_TASK, 
+    # IOI_GPT2_AUTOENCODER_COMPONENT_CIRCUIT_TASK, 
+    # GREATERTHAN_GPT2_AUTOENCODER_COMPONENT_CIRCUIT_TASK, 
+    # ANIMAL_DIET_GPT2_AUTOENCODER_COMPONENT_CIRCUIT_TASK
+]
+use_abs = [True, False]
+ablation_types = [
+    AblationType.RESAMPLE, 
+    AblationType.TOKENWISE_MEAN_CLEAN,
+    AblationType.TOKENWISE_MEAN_CORRUPT, 
+    AblationType.TOKENWISE_MEAN_CLEAN_AND_CORRUPT,
+    AblationType.ZERO
+]
+grad_funcs = [
+    GradFunc.LOGIT, 
+    GradFunc.LOGPROB
+]
+answer_funcs = [
+    AnswerFunc.MAX_DIFF, 
+    AnswerFunc.AVG_VAL
+]
 
-# use_abs_to_epsilon = {
-#     True: [0.1], 
-#     False: [0.0, -0.4]
-# }
+use_abs_to_epsilon = {
+    True: [0.1], 
+    False: [0.0, -0.4]
+}
 
-# # generate all combinations
-# combinations = [
-#     {
-#         "task": f'"{task.key}"',
-#         "use_abs": use_ab,
-#         "ablation_type": ablation_type.name,
-#         "grad_func": grad_func.name,
-#         "answer_func": answer_func.name,
-#         "epsilon": epsilon
-#     }
-#     for task, use_ab, ablation_type, grad_func, answer_func in itertools.product(
-#         tasks,
-#         use_abs,
-#         ablation_types,
-#         grad_funcs,
-#         answer_funcs
-#     )
-#     for epsilon in use_abs_to_epsilon[use_ab]
-# ]
-
+# generate all combinations
 combinations = [
     {
-        "task": f"'{DOCSTRING_TOKEN_CIRCUIT_TASK.key}'",
-        "use_abs": False,
-        "ablation_type": AblationType.TOKENWISE_MEAN_CORRUPT.name,
-        "grad_func": GradFunc.LOGPROB.name,
-        "answer_func": AnswerFunc.AVG_VAL.name,
-        "epsilon": 0.0
+        "task": f"'{task.key}'",
+        "use_abs": use_ab,
+        "ablation_type": ablation_type.name,
+        "grad_func": grad_func.name,
+        "answer_func": answer_func.name,
+        "epsilon": epsilon
     }
+    for task, use_ab, ablation_type, grad_func, answer_func in itertools.product(
+        tasks,
+        use_abs,
+        ablation_types,
+        grad_funcs,
+        answer_funcs
+    )
+    for epsilon in use_abs_to_epsilon[use_ab]
 ]
+
+# combinations = [
+#     {
+#         "task": f"'{IOI_TOKEN_CIRCUIT_TASK.key}'",
+#         "use_abs": False,
+#         "ablation_type": AblationType.TOKENWISE_MEAN_CORRUPT.name,
+#         "grad_func": GradFunc.LOGPROB.name,
+#         "answer_func": AnswerFunc.AVG_VAL.name,
+#         "epsilon": 0.0
+#     }
+# ]
 
 # setup the executor
 out_dir = repo_path_to_abs_path(OUTPUT_DIR / "hypo_test_out_logs" / datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
